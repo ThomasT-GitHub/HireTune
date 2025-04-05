ff85e8e195ef9863629a892210c464cc6d200d6a
Thomas Trivino <th992984@ucf.edu>
2025-04-05 19:02:08 -0400

    Adds bootstrap and react and updated readme to include instructions on how to run project

---
+++ HireTuneSite/frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center">Hello from HACKUSF!</h1>
+              <p className="card-text">Welcome to HireTune with Bootstrap</p>
+              <button className="btn btn-primary">Bootstrap Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
